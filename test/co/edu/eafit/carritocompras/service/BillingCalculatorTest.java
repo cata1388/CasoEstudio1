4ab8d1ffa00eb80042905f7a31f0a499f6c4bc12
Catalina S <cata.1388@gmail.com>
2012-08-29 14:59:06 -0500

    punto 1

--- test/co/edu/eafit/carritocompras/service/BillingCalculatorTest.java
+++ test/co/edu/eafit/carritocompras/service/BillingCalculatorTest.java
@@ -1,5 +1,52 @@
 package co.edu.eafit.carritocompras.service;
 
+import co.edu.eafit.carritocompras.data.Customer;
+import co.edu.eafit.carritocompras.data.Purchase;
+import co.edu.eafit.carritocompras.data.Product;
+import static org.junit.Assert.*;
+import java.math.BigDecimal;
+import org.junit.*;
+//import org.junit.Test;
+//import org.junit.Before;
+
 public class BillingCalculatorTest {
+    
+    private Customer customer;
+   // private BillingCalculator billingCalculator;
+    
+    
+    @Before
+    public void before(){
+        customer = new Customer("a11","b");
+      //  billingCalculator = new BillingCalculator();
+    }
+    /**
+     * Test of calculateTotalPurchase method, of class BillingCalculator.
+     */
+    @Test
+    public void testCalculateTotalPurchase() {
+        String productsFlatFile = "EL-002,FU-007,FU-008";
+        Purchase result = null;
+        BigDecimal discounts = BigDecimal.ZERO;
+        BigDecimal price = BigDecimal.ZERO;
+        result = BillingCalculator.calculateTotalPurchase(customer, productsFlatFile);
+        for (String code : productsFlatFile.split(","))
+        {
+            Product p = Product.buildProduct(code);
+            if (code.startsWith("EL"))
+            {
+                //true values
+                price = price.add(p.getPrice());
+                // just discounts
+                discounts = discounts.add(p.getPrice().multiply(new BigDecimal(0.2)));
+            }
+            else if (code.startsWith("FU"))
+            {
+                price = price.add(p.getPrice());
+                discounts = discounts.add(p.getPrice().multiply(new BigDecimal (0.1)));
+            }
+        }
+        assertEquals(result.getTotalPrice().add(discounts),price);  
+    }
 
 }
--- test/co/edu/eafit/carritocompras/service/PaymentServiceTest.java
+++ test/co/edu/eafit/carritocompras/service/PaymentServiceTest.java
@@ -2,7 +2,7 @@ package co.edu.eafit.carritocompras.service;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 import co.edu.eafit.carritocompras.data.Customer;
 import co.edu.eafit.carritocompras.data.Purchase;
@@ -20,11 +20,11 @@ public class PaymentServiceTest {
 	
 	@Test
 	public void testPay() {
-		GenericCreditCardService creditCardService = Mockito.mock(GenericCreditCardService.class);
+		GenericCreditCardService creditCardService = mock(GenericCreditCardService.class);
 		Purchase p = BillingCalculator.calculateTotalPurchase(customer, "EL-001,FU-002");
 		
 		//Mocking external service behavior
-		Mockito.when(creditCardService.pay("xxxx111xxxx", p.getTotalPrice())).thenReturn(true);
+		when(creditCardService.pay("xxxx111xxxx", p.getTotalPrice())).thenReturn(true);
 		
 		paymentService.pay(customer, p, "xxxx111xxxx", creditCardService);
 		
